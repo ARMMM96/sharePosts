809aca017d90d829040d691b3cff6b517d2070fe
AHMED RABIE MAHMOUD <ahmed.rabie.mahmoud.96@gmail.com>
2022-02-21 03:36:51 +0200

    Form Validation added

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,5 +1,5 @@
 <?php
-class Users extends Controller  {
+class Users extends Controller {
     public function __construct() {
     }
 
@@ -8,6 +8,51 @@ class Users extends Controller  {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Process form
 
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            // Init data
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            ];
+
+            // Validate email
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+            // Validate name
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter name';
+            }
+            // Validate Password
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            } elseif (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+
+            // Validate Confirm Password
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please confirm password';
+            } else {
+                if ($data['password'] != $data['confirm_password']) {
+                    $data['confirm_password_err'] = 'Passwords do not match';
+                }
+            }
+
+            // Make sure errors are empty
+            if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+                die("SUCCESS");
+            } else {
+                // Load view with errors
+                $this->view('users/register', $data);
+            }
         } else {
             // Init data
             $data = [
@@ -30,15 +75,42 @@ class Users extends Controller  {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Process form
 
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            // Init data
+            $data = [
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            // Validate email
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+
+            // Validate Password
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            }
+
+            // Make sure errors are empty
+            if (empty($data['email_err']) && empty($data['password_err'])) {
+                die("SUCCESS");
+            } else {
+                // Load view with errors
+                $this->view('users/login', $data);
+            }
         } else {
             // Init data
             $data = [
-          
+
                 'email' => '',
                 'password' => '',
                 'email_err' => '',
                 'password_err' => '',
-         
+
             ];
 
             // Load view
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -4,23 +4,23 @@
         <div class="card card-body bg-light mt-5">
             <h2>Login</h2>
             <p>Please fill in your credentials to login</p>
-            <form action="<?php echo URLROOT; ?>/users/login" method="POST">
+            <form action="<?php echo URLROOT; ?>/users/login" method="post">
                 <div class="form-group">
                     <label for="email">Email: <sup>*</sup></label>
-                    <input type="email" name="email" class="form-control form-control-lg" <?php echo (!empty($data['email_err'])) ?  'is-invalid' : '' ?> value="<?php echo $data['email'] ?>">
-                    <span class="invalid-feedback"><?php echo $data['email_err'] ?></span>
+                    <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ?  'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+                    <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
                 </div>
                 <div class="form-group">
                     <label for="password">Password: <sup>*</sup></label>
-                    <input type="password" name="password" class="form-control form-control-lg" <?php echo (!empty($data['password_err'])) ?  'is-invalid' : '' ?> value="<?php echo $data['password'] ?>">
-                    <span class="invalid-feedback"><?php echo $data['password_err'] ?></span>
+                    <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ?  'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
+                    <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
                 </div>
                 <div class="row">
                     <div class="col">
                         <input type="submit" value="Login" class="btn btn-success btn-block">
                     </div>
                     <div class="col">
-                        <a href="<?php echo URLROOT; ?>/users/register" class="btn btn-lgiht btn-block">No an account ? Register</a>
+                        <a href="<?php echo URLROOT; ?>/users/register" class="btn btn-light btn-block">No account ? Register</a>
                     </div>
                 </div>
             </form>
